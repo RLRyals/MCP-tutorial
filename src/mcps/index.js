4e80b1d9ba4fa2886f7308aed8cea0ca62ea98a0
RLRyals <hello@rebeccaryals.com>
2025-09-13 04:11:29 -0500

    Update index.js

--- src/mcps/index.js
+++ src/mcps/index.js
@@ -1,6 +1,6 @@
 // src/mcps/index.js
 export { AuthorMCPServer } from './author-server/index.js';
-export { CharacterMCPServer } from './character-server/index.js;
+export { CharacterMCPServer } from './character-server/index.js';
 export { SeriesMCPServer } from './series-server/index.js';
 export { BookMCPServer } from './book-server/index.js';
 export { TimelineMCPServer } from './timeline-server/index.js';
